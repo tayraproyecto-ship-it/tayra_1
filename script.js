e46ca94b8f75292eb1c918efdbb17d16cf5c62a4
tayraproyecto-ship-it <tayraproyecto@gmail.com>
2026-05-26 20:48:05 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function mostrarFormulario(){
+
+    document
+        .getElementById("inicio")
+        .classList.add("hidden");
+
+    document
+        .getElementById("formulario")
+        .classList.remove("hidden");
+}
+
+function guardarNombre(){
+
+    let nombre =
+        document.getElementById("nombre").value;
+
+    if(nombre.trim() === ""){
+
+        alert("Ingresa tu nombre");
+        return;
+    }
+
+    // GUARDAR NOMBRE
+
+    localStorage.setItem("usuario", nombre);
+
+    // IR A OTRA PAGINA
+
+    window.location.href = "hierbas.html";
+}
